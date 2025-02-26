81b6ca08e11a971c94a22f380737bcd281f803f1
Irakli Khitiri <khitiri.irakli@gmail.com>
2025-02-26 10:20:24 +0100

    push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/tierheim-webseite/", // GitHub Pages benötigt den Repository-Namen hier
+  base: "/tierheim-webseite/",
+  build: {
+    outDir: "dist"
+  }
 });
