879664230319c9bfeff9821a2e137cccbb581f3a
Irakli Khitiri <khitiri.irakli@gmail.com>
2025-02-27 13:54:05 +0100

    Deploy to root of main branch

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/tierheim-webseite/',
+  base: '/tierheim-webseite/', // Dies bleibt gleich für GitHub Pages
   build: {
-    outDir: 'dist',
+    outDir: './', // Build in root directory
+    emptyOutDir: false // Verhindert, dass Vite deine Quelldateien löscht
   },
 });
