bd2c561c4ddd7822e8b6ccd24976ffa6ebf5028d
Irakli Khitiri <khitiri.irakli@gmail.com>
2025-02-27 21:52:05 +0100

    ...

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/tierheim-webseite/",
+  
   plugins: [react()],
+  base: "", // NICHT "/tierheim-webseite/" für lokalen Betrieb!
 });
 
